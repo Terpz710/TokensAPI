41a4c8a918291f2bab29bc98a35289b66575c1f1
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2024-03-09 08:21:45 -0800

    Update AddTokens.php

--- src/Terpz710/TokensAPI/Commands/AddTokens.php
+++ src/Terpz710/TokensAPI/Commands/AddTokens.php
@@ -6,11 +6,13 @@ namespace Terpz710\TokensAPI\Commands;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginOwned
 use pocketmine\player\Player;
 
 use Terpz710\TokensAPI\Tokens;
 
-class AddTokens extends Command {
+class AddTokens extends Command implements PluginOwned {
 
     /** @var Tokens */
     private $plugin;
@@ -19,6 +21,10 @@ class AddTokens extends Command {
         parent::__construct("addtoken", "Add tokens to a player's balance", "/addtoken <player> <amount>");
         $this->setPermission("tokensapi.cmd.addtoken");
         $this->plugin = $plugin;
+    }
+
+    public function getOwningPlugin(): Plugin {
+        return $this->plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
