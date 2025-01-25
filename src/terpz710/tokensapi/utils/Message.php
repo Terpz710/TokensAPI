c816764b52e0ab1c19c41a53fc2529ed76093e52
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-01-24 18:58:31 -0800

    Update Message.php

--- src/terpz710/tokensapi/utils/Message.php
+++ src/terpz710/tokensapi/utils/Message.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace terpz710\tokensapi\utils;
 
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TextColor;
+
+use function mkdir;
+use function is_dir;
+use function str_replace;
 
 use terpz710\tokensapi\TokensAPI;
 
@@ -13,7 +18,31 @@ class Message {
     private string $message;
 
     public function __construct(string $msgKey, array|string|null $tags = null, array|string|null $replacements = null) {
-        $msg = TokensAPI::getInstance()->messages->get($msgKey);
+        $lang = TokensAPI::getInstance()->getConfig()->get("language");
+
+        $languageFolder = TokensAPI::getInstance()->getDataFolder() . "languages/";
+
+        if (!is_dir($languageFolder)) {
+            mkdir($languageFolder, 0777, true);
+        }
+
+        if ($lang === "en") {
+            $languageConfig = new Config($languageFolder . "english_messages.yml");
+        } elseif ($lang === "es") {
+            $languageConfig = new Config($languageFolder . "spanish_messages.yml");
+        } elseif ($lang === "fr") {
+            $languageConfig = new Config($languageFolder . "german_messages.yml");
+        } elseif ($lang === "de") {
+            $languageConfig = new Config($languageFolder . "french_messages.yml");
+        } elseif ($lang === "zh-t") {
+            $languageConfig = new Config($languageFolder . "traditional_chinese_messages.yml");
+        } elseif ($lang === "zh-s") {
+            $languageConfig = new Config($languageFolder . "simplified_chinese_messages.yml");
+        } else {
+            throw new \InvalidArgumentException("Invalid language type specified in the configuration: " . $lang);
+        }
+
+        $msg = $languageConfig->get($msgKey);
 
         if ($tags !== null && $replacements !== null) {
             $tags = (array) $tags;
@@ -22,7 +51,7 @@ class Message {
             $msg = str_replace($tags, $replacements, $msg);
         }
 
-        $this->message = TextFormat::colorize($msg);
+        $this->message = TextColor::colorize($msg);
     }
 
     public function __toString() : string{
