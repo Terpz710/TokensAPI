b7a3fdb7c05160b22da32d2233ae598b7b7d8faa
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-02-09 22:14:19 -0800

    Add files via upload

---
+++ src/terpz710/tokensapi/event/RemoveTokenEvent.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace terpz710\tokensapi\event;
+
+use pocketmine\player\Player;
+
+class RemoveTokenEvent extends TokenEvent {
+
+    public function __construct(Player|string $player, int $oldBalance, int $newBalance) {
+        parent::__construct($player, $oldBalance, $newBalance);
+    }
+
+    public function getChangeType() : string{
+        return "remove";
+    }
+}
